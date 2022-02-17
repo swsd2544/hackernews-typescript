42375781cafb9f569e5cf3a95e0e7735332c539c
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2022-02-17 15:12:29 +0700

    Getting Started

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { ApolloServer } from 'apollo-server';
+
+import { schema } from './schema';
+export const server = new ApolloServer({
+	schema,
+});
+
+const port = process.env.PORT || 3000;
+server.listen({ port }).then(({ url }) => {
+	console.log(`🚀  Server ready at ${url}`);
+});
