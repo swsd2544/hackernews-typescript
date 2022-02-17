9dea723aab1bcee0223038b4a4525c443fbddcf4
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2022-02-18 00:27:13 +0700

    Authentication

--- src/context.ts
+++ src/context.ts
@@ -1,10 +1,22 @@
+import { Request } from 'express';
 import { PrismaClient } from '@prisma/client';
+import { decodeAuthHeader } from './utils/auth';
+
 export const prisma = new PrismaClient();
 
 export interface Context {
 	prisma: PrismaClient;
+	userId?: number;
 }
 
-export const context: Context = {
-	prisma,
+export const context = ({ req }: { req: Request }): Context => {
+	const token =
+		req && req.headers.authorization
+			? decodeAuthHeader(req.headers.authorization)
+			: null;
+
+	return {
+		prisma,
+		userId: token?.userId,
+	};
 };
